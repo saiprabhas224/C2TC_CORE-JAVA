1fce19fc374caaa554a8c297c47f5d6588480e09
saipr <saipr@LAPTOP-QUS522D9>
2022-05-26 10:00:19 +0530

    first commit

---
+++ Demo1/src/Ait.java
@@ -0,0 +1,8 @@
+class Ait{
+	int x;
+	public static void main(String[] args) {
+		Ait myObj = new Ait();
+		myObj.x = 40;
+		System.out.println(myObj.x);
+}
+}
---
+++ Demo1/src/hello.java
@@ -0,0 +1,11 @@
+class hello{
+	public static void main(String[] args) {
+		TestDemo myObj = new TestDemo();
+		soft.x = 40;
+		System.out.println(soft.x);
+
+}
+public class 
+
+}
+}
